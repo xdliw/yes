642267e43972cef71d4883b81a4f53e3762f5227
xdliw <90445100+xdliw@users.noreply.github.com>
2023-02-19 19:51:27 +0300

    added func cpp and sum

---
+++ cmake-build-debug/CMakeFiles/tryhard.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tryhard.dir/func.cpp.obj"
+  "CMakeFiles/tryhard.dir/func.cpp.obj.d"
+  "CMakeFiles/tryhard.dir/main.cpp.obj"
+  "CMakeFiles/tryhard.dir/main.cpp.obj.d"
+  "libtryhard.dll.a"
+  "tryhard.exe"
+  "tryhard.exe.manifest"
+  "tryhard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tryhard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/prog/ahhTesting/tryhard
+# Install script for directory: D:/prog/ahhTesting/takinghard/yes
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/prog/ahhTesting/tryhard/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/prog/ahhTesting/takinghard/yes/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
